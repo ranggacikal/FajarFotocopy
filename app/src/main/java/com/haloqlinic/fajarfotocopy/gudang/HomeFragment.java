b8aa038502a076c20a57f443539c073b1bb087d2
ranggacikal <ranggacikal2@gmail.com>
2021-04-10 23:17:31 +0700

    pindah directory

--- app/src/main/java/com/haloqlinic/fajarfotocopy/gudang/HomeFragment.java
+++ app/src/main/java/com/haloqlinic/fajarfotocopy/gudang/HomeFragment.java
@@ -1,4 +1,4 @@
-package com.haloqlinic.fajarfotocopy;
+package com.haloqlinic.fajarfotocopy.gudang;
 
 import android.os.Bundle;
 
@@ -7,6 +7,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.haloqlinic.fajarfotocopy.R;
 
 /**
  * A simple {@link Fragment} subclass.
