d69e2b94c37749d0f4ee80734067b5e59705687c
ranggacikal <ranggacikal2@gmail.com>
2021-04-10 23:53:04 +0700

    Update nih

--- app/src/main/java/com/haloqlinic/fajarfotocopy/MainActivity.java
+++ app/src/main/java/com/haloqlinic/fajarfotocopy/MainActivity.java
@@ -4,11 +4,34 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.android.material.tabs.TabLayout;
+
 public class MainActivity extends AppCompatActivity {
+
+    TabLayout tabLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getAllWidgets();
+        setupTabLayout();
     }
+
+    private void setupTabLayout() {
+
+        tabLayout.addTab(tabLayout.newTab().setText("Home").setIcon(R.drawable.ic_home));
+        tabLayout.addTab(tabLayout.newTab().setText("Informasi").setIcon(R.drawable.ic_informasi));
+        tabLayout.addTab(tabLayout.newTab().setText("Kirim Barang").setIcon(R.drawable.ic_kirim));
+        tabLayout.addTab(tabLayout.newTab().setText("Keranjang").setIcon(R.drawable.ic_keranjang));
+
+    }
+
+    private void getAllWidgets() {
+
+        tabLayout = (TabLayout) findViewById(R.id.tab_layout_gudang);
+    }
+
+
 }
