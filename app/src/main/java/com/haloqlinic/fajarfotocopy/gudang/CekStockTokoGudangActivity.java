aa8548f9b333c0d3568043dd34f88dbdd5bfd1c4
DENI <denipamungkas13@gmail.com>
2021-04-11 00:47:04 +0700

    Toko Gudang

---
+++ app/src/main/java/com/haloqlinic/fajarfotocopy/gudang/CekStockTokoGudangActivity.java
@@ -0,0 +1,14 @@
+package com.haloqlinic.fajarfotocopy.gudang;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CekStockTokoGudangActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cek_stock_toko_gudang);
+    }
+}
