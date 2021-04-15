56dbfdf7ab61dacc89d9fb088778d3ebdb5b1cd9
radentech <radentechnologysolution@gmail.com>
2021-04-15 23:37:13 +0700

    layout detail barang gudang layout tambah toko gudang layout data pertoko gudang layout data toko

--- app/src/main/java/com/haloqlinic/fajarfotocopy/gudang/LoginActivity.java
+++ app/src/main/java/com/haloqlinic/fajarfotocopy/gudang/LoginActivity.java
@@ -1,8 +1,10 @@
-package com.haloqlinic.fajarfotocopy;
+package com.haloqlinic.fajarfotocopy.gudang;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.haloqlinic.fajarfotocopy.R;
 
 public class LoginActivity extends AppCompatActivity {
 
