2943e8eb63fe1d2168dfa29ed63e4c474f7e7f2f
radentech <radentechnologysolution@gmail.com>
2021-04-18 00:02:33 +0700

    layout supplier gudang keranjang supplier gudang pembayaran supplier gudang cetak bukti supplier gudang

---
+++ app/src/main/java/com/haloqlinic/fajarfotocopy/gudang/suppliergudang/SupplierGudangActivity.java
@@ -0,0 +1,16 @@
+package com.haloqlinic.fajarfotocopy.gudang.suppliergudang;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.haloqlinic.fajarfotocopy.R;
+
+public class SupplierGudangActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_supplier_gudang);
+    }
+}
