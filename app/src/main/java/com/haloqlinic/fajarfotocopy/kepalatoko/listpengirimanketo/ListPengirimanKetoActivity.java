b8339711e6a7bd75fd4ab13d8fcd72212386bc1c
DENI <denipamungkas13@gmail.com>
2021-06-19 21:11:33 +0700

    19 juni 2021

---
+++ app/src/main/java/com/haloqlinic/fajarfotocopy/kepalatoko/listpengirimanketo/ListPengirimanKetoActivity.java
@@ -0,0 +1,14 @@
+package com.haloqlinic.fajarfotocopy.kepalatoko.listpengirimanketo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ListPengirimanKetoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_pengiriman_keto);
+    }
+}
