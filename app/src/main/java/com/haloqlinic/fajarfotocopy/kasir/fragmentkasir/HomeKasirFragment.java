e66e7ea38b648ca70570fa350c5e340e0ba9c86b
DENI <denipamungkas13@gmail.com>
2021-06-26 02:45:02 +0700

    26 juni 2021

--- app/src/main/java/com/haloqlinic/fajarfotocopy/kasir/fragmentkasir/HomeKasirFragment.java
+++ app/src/main/java/com/haloqlinic/fajarfotocopy/kasir/fragmentkasir/HomeKasirFragment.java
@@ -6,6 +6,9 @@ import android.os.Bundle;
 
 import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -18,12 +21,15 @@ import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 import com.haloqlinic.fajarfotocopy.LoginActivity;
 import com.haloqlinic.fajarfotocopy.R;
 import com.haloqlinic.fajarfotocopy.SharedPreference.SharedPreferencedConfig;
+import com.haloqlinic.fajarfotocopy.api.ConfigRetrofit;
 import com.haloqlinic.fajarfotocopy.gudang.tokogudang.TokoGudangActivity;
 import com.haloqlinic.fajarfotocopy.kasir.transaksikasir.TransaksiKasirActivity;
+import com.haloqlinic.fajarfotocopy.model.tambahStatusPenjualan.ResponseTambahStatusPenjualan;
 import com.thekhaeng.pushdownanim.PushDownAnim;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Random;
 
 import static com.thekhaeng.pushdownanim.PushDownAnim.MODE_SCALE;
 
@@ -69,6 +75,7 @@ public class HomeKasirFragment extends Fragment {
 
         calendar = Calendar.getInstance();
         dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+
         date = dateFormat.format(calendar.getTime());
 
         txtTanggal.setText(date);
@@ -78,7 +85,7 @@ public class HomeKasirFragment extends Fragment {
                 .setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-                        startActivity(new Intent(getActivity(), TransaksiKasirActivity.class));
+                        tambahStatusPenjualan();
                     }
                 });
 
@@ -108,6 +115,46 @@ public class HomeKasirFragment extends Fragment {
                 });
         return rootView;
 
+
+    }
+
+    private void tambahStatusPenjualan() {
+        Random rnd = new Random();
+        int number = rnd.nextInt(999999);
+
+        String randomId = String.format("%06d", number);
+        String id_status_penjualan = "SPN"+randomId;
+        String status_penjualan = "pending";
+
+        calendar = Calendar.getInstance();
+        dateFormat = new SimpleDateFormat("dd MMMM yyyy");
+
+        date = dateFormat.format(calendar.getTime());
+        String tanggal = date;
+        String id_outlet = preferencedConfig.getPreferenceIdOutlet();
+
+        ConfigRetrofit.service.tambahStatusPenjualan(id_status_penjualan, status_penjualan, tanggal, id_outlet)
+                .enqueue(new Callback<ResponseTambahStatusPenjualan>() {
+                    @Override
+                    public void onResponse(Call<ResponseTambahStatusPenjualan> call, Response<ResponseTambahStatusPenjualan> response) {
+                        if(response.isSuccessful()){
+                            int status = response.body().getStatus();
+                            if(status == 1){
+                                startActivity(new Intent(getActivity(), TransaksiKasirActivity.class));
+                            }else {
+                                Toast.makeText(getContext(), "Gagal Membuat Penjualan", Toast.LENGTH_SHORT).show();
+                            }
+                        }else {
+                            Toast.makeText(getContext(), "Terjadi Kesalahan Di Server", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(Call<ResponseTambahStatusPenjualan> call, Throwable t) {
+                        Toast.makeText(getContext(), "Error : "+t.getMessage(), Toast.LENGTH_SHORT).show();
+
+                    }
+                });
 
     }
 
--- app/src/main/java/com/haloqlinic/fajarfotocopy/kasir/transaksikasir/TransaksiKasirActivity.java
+++ app/src/main/java/com/haloqlinic/fajarfotocopy/kasir/transaksikasir/TransaksiKasirActivity.java
@@ -1,22 +1,116 @@
 package com.haloqlinic.fajarfotocopy.kasir.transaksikasir;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.haloqlinic.fajarfotocopy.R;
+import com.haloqlinic.fajarfotocopy.api.ConfigRetrofit;
+import com.haloqlinic.fajarfotocopy.model.getIdStatusPenjualan.ResponseGetIdStatusPenjualan;
+import com.haloqlinic.fajarfotocopy.model.hapusStatusPenjualan.ResponseHapusStatusPenjualan;
+
+import androidx.appcompat.app.AppCompatActivity;
+import dev.shreyaspatil.MaterialDialog.BottomSheetMaterialDialog;
+import dev.shreyaspatil.MaterialDialog.interfaces.DialogInterface;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class TransaksiKasirActivity extends AppCompatActivity {
+
+    String id_status_penjualan;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_transaksi_kasir);
+
+        getStatusPenjualan();
+    }
+
+    private void getStatusPenjualan() {
+        ConfigRetrofit.service.getIdStatusPenjualan().enqueue(new Callback<ResponseGetIdStatusPenjualan>() {
+            @Override
+            public void onResponse(Call<ResponseGetIdStatusPenjualan> call, Response<ResponseGetIdStatusPenjualan> response) {
+                if (response.isSuccessful()) {
+                    int status = response.body().getStatus();
+                    if (status == 1) {
+                        id_status_penjualan = response.body().getDataIdStatusPenjualan().getIdStatusPenjualan();
+
+                    } else {
+                        Toast.makeText(TransaksiKasirActivity.this, "Gagal Mengambil Id Status Penjualan", Toast.LENGTH_SHORT).show();
+                    }
+                } else {
+                    Toast.makeText(TransaksiKasirActivity.this, "Terjadi Kesalahan Di Server", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<ResponseGetIdStatusPenjualan> call, Throwable t) {
+                Toast.makeText(TransaksiKasirActivity.this, "error" + t.getMessage(), Toast.LENGTH_SHORT).show();
+
+            }
+        });
     }
 
     @Override
     public void onBackPressed() {
-        super.onBackPressed();
-        finish();
+
+        BottomSheetMaterialDialog mBottomSheetDialog = new BottomSheetMaterialDialog.Builder(TransaksiKasirActivity.this)
+                .setTitle("Batal Transaksi?")
+                .setMessage("Apakah anda yakin ingin membatalkan transaksi ini?")
+                .setCancelable(false)
+                .setPositiveButton("Iya", new BottomSheetMaterialDialog.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int which) {
+
+                        TransaksiKasirActivity.super.onBackPressed();
+
+                        hapusStatusPenjualan();
+                        dialogInterface.dismiss();
+                    }
+                })
+                .setNegativeButton("Tidak", new BottomSheetMaterialDialog.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int which) {
+                        dialogInterface.dismiss();
+                    }
+                })
+                .build();
+
+        // Show Dialog
+        mBottomSheetDialog.show();
+
+    }
+
+
+    private void hapusStatusPenjualan() {
+        ProgressDialog progressDialog = new ProgressDialog(TransaksiKasirActivity.this);
+        progressDialog.setMessage("Membatalkan Transaksi");
+        progressDialog.show();
+        ConfigRetrofit.service.hapusStatusPenjualan(id_status_penjualan).enqueue(new Callback<ResponseHapusStatusPenjualan>() {
+            @Override
+            public void onResponse(Call<ResponseHapusStatusPenjualan> call, Response<ResponseHapusStatusPenjualan> response) {
+                if (response.isSuccessful()) {
+                    progressDialog.dismiss();
+                    int status = response.body().getStatus();
+                    if (status == 1) {
+                        finish();
+                    } else {
+                        Toast.makeText(TransaksiKasirActivity.this, "Gagal Membatalkan Transaksi", Toast.LENGTH_SHORT).show();
+                    }
+                } else {
+                    progressDialog.dismiss();
+                    Toast.makeText(TransaksiKasirActivity.this, "Terjadi Kesalahan Di Server", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<ResponseHapusStatusPenjualan> call, Throwable t) {
+                progressDialog.dismiss();
+                Toast.makeText(TransaksiKasirActivity.this, "Terjadi Kesalahan Di Server" + t.getMessage(), Toast.LENGTH_SHORT).show();
+
+            }
+        });
     }
 }
